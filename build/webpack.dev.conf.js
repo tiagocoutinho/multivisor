f55b74d60fc810d9d6cb869c7cc04e60512e46d7
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2018-02-13 20:00:17 +0100

    webpack: make SSE work with webpack dev server

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -30,7 +30,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     },
     hot: true,
     contentBase: false, // since we use CopyWebpackPlugin.
-    compress: true,
+    compress: false,
     disableHostCheck: true,
     host: HOST || config.dev.host,
     port: PORT || config.dev.port,
