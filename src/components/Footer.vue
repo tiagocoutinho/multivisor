10a69808748d6dd59c55f20abf05a14e91759fe7
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2018-02-24 16:08:04 +0100

    frontend: add footer (not being used yet)

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-footer dark app class="info">
+    <v-layout row wrap justify-center>
+      <v-flex xs12 py-3 text-xs-center white--text>
+        <ProcessChip></ProcessChip>
+        <SupervisorChip></SupervisorChip>
+      </v-flex>
+    </v-layout>
+  </v-footer>
+</template>
+
+<script>
+import ProcessChip from './process/Chip'
+import SupervisorChip from './supervisor/Chip'
+
+export default { components: { ProcessChip, SupervisorChip } }
+</script>
