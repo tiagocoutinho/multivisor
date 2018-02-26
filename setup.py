6f21754d41864a351072e5ee52be03680a544126
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2018-02-26 08:53:42 +0100

    version 3.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='multivisor',
-    version='3.0.1',
+    version='3.0.2',
     author='Tiago Coutinho',
     author_email='coutinhotiago@gmail.com',
     description='A centralized supervisor web UI',
