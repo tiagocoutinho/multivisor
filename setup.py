7e9d079db3af0442f7ef684e10fd1164e3216b6f
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2018-05-18 15:48:34 +0200

    Prepare 4.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='multivisor',
-    version='4.1.1',
+    version='4.1.2',
     author='Tiago Coutinho',
     author_email='coutinhotiago@gmail.com',
     description='A centralized supervisor web UI',
