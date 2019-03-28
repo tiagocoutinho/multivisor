406d009ac29f66a82b12e48c06936aafd02ef546
Paweł <pawel.mucha@yahoo.pl>
2019-03-28 17:01:04 +0100

    Add basic login page view.

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,14 @@ import Router from 'vue-router'
 import GroupPage from '@/components/group/Page'
 import ProcessPage from '@/components/process/Page'
 import SupervisorPage from '@/components/supervisor/Page'
+import LoginPage from '@/components/login/Page'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {path: '/', name: 'Home', component: GroupPage},
+    {path: '/login', name: 'login', component: LoginPage},
     {path: '/view/group', name: 'GroupPage', component: GroupPage},
     {path: '/view/supervisor', name: 'SupervisorPage', component: SupervisorPage},
     {path: '/view/process', name: 'ProcessPage', component: ProcessPage}
