c769ebf33d478719d17ee6ef52620bd7d3d260ae
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2020-11-22 19:55:00 +0100

    setup: manage dependencies and separate project into 3 components

--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
+import sys
 from setuptools import setup, find_packages
+
+
+supervisor = 'supervisor-win' if 'win' in sys.platform else 'supervisor'
+
+extras = {
+    'rpc': ['zerorpc', supervisor],
+    'web': ['flask', 'werkzeug', 'blinker', 'zerorpc', supervisor],
+    'cli': ['maya', 'requests', 'prompt_toolkit>=2', 'blinker'],
+}
+
+extras['all'] = list(set.union(*(set(i) for i in extras.values())))
+
+requires = ['six', 'gevent>=1.3']
 
 setup(
     name='multivisor',
@@ -11,7 +25,7 @@ setup(
                                         'dist/static/css/*',
                                         'dist/static/js/*']},
     entry_points=dict(console_scripts=[
-        'multivisor=multivisor.server.web:main',
-        'multivisor-cli=multivisor.client.cli:main']),
-    install_requires=['flask', 'gevent>=1.3', 'supervisor', 'zerorpc', 'blinker',
-                      'maya', 'requests', 'prompt_toolkit>=2'])
+        'multivisor=multivisor.server.web:main [web]',
+        'multivisor-cli=multivisor.client.cli:main [cli]']),
+    extras_require=extras,
+    install_requires=requires)
