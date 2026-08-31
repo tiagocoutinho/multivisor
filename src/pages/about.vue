d0f3e4bd92f397dc9e7f41341fe2034bed809578
Samuel Debionne <samuel.debionne@esrf.fr>
2026-08-31 18:44:53 +0200

    frontend: add about page

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,3 +1,33 @@
 <template>
-  <h1>About</h1>
+  <v-container>
+    <h1>About</h1>
+    A centralized supervisor UI (Web & CLI)
+
+    <h2>Features</h2>
+    <ul>
+      <li>Processes status always up to date</li>
+      <li>Reactivity through asynchronous actions</li>
+      <li>Notifications when state changes</li>
+      <li>Mobile aware, SPA web page</li>
+      <li>Powerful filters</li>
+      <li>Interactive CLI</li>
+      <li>Works on supervisor and supervisor-win</li>
+    </ul>
+
+    <h2>Components</h2>
+    Multivisor is comprised of 3 components:
+    <ul>
+      <li>
+        WEB server: gathers information from all supervisors and provides a
+        dashboard like UI to the entire system multivisor
+      </li>
+      <li>
+        RPC : an RPC extension to supervisor used to communicate between each
+        supervisord and multivisor web server
+      </li>
+      <li>
+        CLI: an optional CLI which communicates with multivisor web server
+      </li>
+    </ul>
+  </v-container>
 </template>
